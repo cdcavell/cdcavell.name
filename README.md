b54e9f2c67db67aa27d2e3c2bbc8ed6d4d154e8b
Christopher Cavell <cdcavell@gmail.com>
2020-09-28 09:48:09 -0500

    1.0.0 - Initial Development

---
+++ README.md
@@ -0,0 +1,36 @@
+# [cdcavell.name](https://cdcavell.name)
+Personal Website for Christopher D. Cavell
+<hr />
+
+![GitHub top language](https://img.shields.io/github/languages/top/cdcavell/cdcavell.name?style=flat-square)
+
+<hr />
+
+Project incorporates generation of markdown files in Documentation folder, during project builds, from comment syntax of source code, through console application XmlToMarkdown. Documentation changes are maintained in a [wiki submodule](https://brendancleary.com/2013/03/08/including-a-github-wiki-in-a-repository-as-a-submodule/) that is also updated during project build.
+
+Target Frameworks are [.NET Core 3.1](https://dotnet.microsoft.com/download/dotnet-core/3.1) and [.NET Standard 2.0](https://docs.microsoft.com/en-us/dotnet/standard/net-standard) respectfully. Developed and built in a Windows environment utilizing [Visual Studio Community 2019 ](https://visualstudio.microsoft.com/vs/) source-code editor. Repository is [Git](https://git-scm.com/) utilizing [git-flow](https://danielkummer.github.io/git-flow-cheatsheet/) extention to provide high-level repository operations for [Vincent Driessen's branching model](https://nvie.com/posts/a-successful-git-branching-model/).
+
+All work is [licensed](https://github.com/cdcavell/cdcavell.name/blob/master/LICENSE) under the [MIT License](https://opensource.org/licenses/MIT). Source Code documentation is found in repository [Wiki](https://github.com/cdcavell/cdcavell.name/wiki) section.
+
+<hr />
+
+_If you are cloning this repository, please enter commands as follows:_
+
+```
+$ git clone --recurse-submodules https://github.com/cdcavell/cdcavell.name.git
+
+$ cd cdcavell.name
+
+$ git flow init -d
+```
+
+<hr />
+
+
+
+[__Website__](https://cdcavell.name) deployment 
+
+| Build | Date | Description |
+|-------|------|-------------|
+| 1.0.0 | 09/28/2020 | __Initial Development__ |
+
---
+++ Source/Console/XmlToMarkdown/Program.cs
@@ -0,0 +1,124 @@
+﻿using CDCavell.ClassLibrary.Commons;
+using CDCavell.ClassLibrary.Commons.Xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace XmlToMarkdown
+{
+    /// <summary>
+    /// Command console program to transform all xml files in current directory utilizing supplied 
+    /// template file to Wiki Markdown files.
+    /// 
+    /// Usage: dotnet XmlToMarkdown.dll [Template file]
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 1.0.0 | 07/05/2020 | Initial build |~ 
+    /// </revision>
+    class Program
+    {
+        private static List<KeyValuePair<string, string>> kvpList = new List<KeyValuePair<string, string>>();
+
+        /// <summary>
+        /// Entry point method
+        /// </summary>
+        /// <param name="args">string[]</param>
+        /// <method>Main(string[] args)</method>
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: dotnet XmlToMarkdown.dll [Template file]");
+                return;
+            }
+
+            char[] anyOf = { '\\', '/' };
+            string templateFile = args[0];
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(" Process started");
+            Console.WriteLine(string.Empty);
+
+            try
+            {
+                ClearWiki();
+
+                foreach (string file in Directory.EnumerateFiles(Directory.GetCurrentDirectory()))
+                {
+                    int extIndex = file.LastIndexOf(".xml", StringComparison.CurrentCultureIgnoreCase);
+                    int slashIndex = file.LastIndexOfAny(anyOf);
+
+                    if (extIndex > 0)
+                    {
+                        string fileNameOnly = file.Substring(slashIndex + 1);
+                        fileNameOnly = fileNameOnly.Substring(0, fileNameOnly.LastIndexOf(".xml", StringComparison.CurrentCultureIgnoreCase));
+                        Console.Write(" Processing " + fileNameOnly + ".xml");
+
+                        string outputFile = Directory.GetCurrentDirectory() + "/wiki/" + fileNameOnly + ".md";
+                        if (File.Exists(outputFile))
+                            File.Delete(outputFile);
+
+                        Transform.Write(Directory.GetCurrentDirectory() + "/" + templateFile, file, outputFile);
+
+                        kvpList.Add(new KeyValuePair<string, string>(fileNameOnly, fileNameOnly));
+                        Console.WriteLine(" --> Generated " + fileNameOnly + ".md");
+                    }
+                }
+
+                if (kvpList.Count > 0)
+                    WriteHome();
+
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(" Process completed");
+                Console.WriteLine(string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void WriteHome()
+        {
+            Console.WriteLine(" Generating Home.md");
+            string homeFile = Directory.GetCurrentDirectory() + "/wiki/Home.md";
+            if (File.Exists(homeFile))
+                File.Delete(homeFile);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(AsciiCodes.CRLF);
+            sb.Append("| Project Source Code Documentation |");
+            sb.Append(AsciiCodes.CRLF);
+            sb.Append("|-----------------------------------|");
+            sb.Append(AsciiCodes.CRLF);
+
+            kvpList.Sort((x, y) => x.Key.CompareTo(y.Key));
+            foreach (KeyValuePair<string, string> kvp in kvpList)
+            {
+                sb.Append("| [" + kvp.Key + "](" + kvp.Value + ") |");
+                sb.Append(AsciiCodes.CRLF);
+            }
+
+            File.WriteAllText(homeFile, sb.ToString());
+
+            Console.WriteLine(" Generated Home.md");
+        }
+
+        private static void ClearWiki()
+        {
+            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory() + "/wiki/");
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                if (file.FullName.Contains(".md"))
+                {
+                    Console.WriteLine(" Removing " + file.FullName);
+                    file.Delete();
+                }
+            }
+        }
+    }
+}
