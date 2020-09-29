e564e0b261b3e3d044ed49469c46b8d70024ccec
Christopher Cavell <cdcavell@gmail.com>
2020-09-29 13:36:18 -0500

    1.0.0 - Initial Development

---
+++ Source/Web/is4-cdcavell/Config.cs
@@ -0,0 +1,69 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace is4_cdcavell
+{
+    /// <summary>
+    /// IdentityServer4 In Memory Configuration
+    /// &lt;br /&gt;&lt;br /&gt;
+    /// Copyright (c) Brock Allen &amp; Dominick Baier. All rights reserved.
+    /// Licensed under the Apache License, Version 2.0. 
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 1.0.0 | 09/29/2020 | Initial build |~ 
+    /// </revision>
+    public static class Config
+    {
+        /// <value>IEnumerable&lt;IdentityResource&gt;</value>
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        /// <value>IEnumerable&lt;ApiScope&gt;</value>
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        /// <value>IEnumerable&lt;Client&gt;</value>
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
+                },
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                },
+            };
+    }
+}
--- Source/Web/is4-cdcavell/Startup.cs
+++ Source/Web/is4-cdcavell/Startup.cs
@@ -66,6 +66,28 @@ namespace is4_cdcavell
             services.AddScoped<ControllerActionLogFilter>();
             services.AddScoped<ControllerActionUserFilter>();
             services.AddScoped<ControllerActionPageFilter>();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddTestUsers(TestUsers.Users);
+
+            // in-memory, code config
+            builder.AddInMemoryIdentityResources(Config.IdentityResources);
+            builder.AddInMemoryApiScopes(Config.ApiScopes);
+            builder.AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication();
         }
 
         /// <summary>
@@ -97,9 +119,7 @@ namespace is4_cdcavell
             app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseCors();
-
-            //app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
