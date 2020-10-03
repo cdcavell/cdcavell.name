79a1515e380b17cc4232792ee24772d9c7c70c3e
Christopher Cavell <cdcavell@gmail.com>
2020-10-03 08:18:38 -0500

    1.0.0 - Initial Development

--- Source/Web/cdcavell/Controllers/AdministrationController.cs
+++ Source/Web/cdcavell/Controllers/AdministrationController.cs
@@ -13,7 +13,7 @@ namespace cdcavell.Controllers
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 1.0.0 | 10/02/2020 | Initial build |~ 
+    /// | Christopher D. Cavell | 1.0.0 | 10/03/2020 | Initial build |~ 
     /// </revision>
     public class AdministrationController : WebBaseController<AdministrationController>
     {
