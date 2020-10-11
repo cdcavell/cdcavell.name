c5f0ec3b75e9fcb52f062f909bcedcd26c2e3415
Christopher Cavell <cdcavell@gmail.com>
2020-10-10 19:04:38 -0500

    1.0.0 - Initial Development

---
+++ Source/Web/cdcavell/Models/Search/SearchModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cdcavell.Models.Search
+{
+    /// <summary>
+    /// Search Input Model
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 1.0.0 | 10/10/2020 | Initial build |~ 
+    /// </revision>
+    public class SearchModel
+    {
+        /// <value>string</value>
+        [Required]
+        public string SearchRequest { get; set; }
+        /// <value>string</value>
+        public string MessageClass { get; set; } = "text-info";
+        /// <value>string</value>
+        public string Message { get; set; }
+    }
+}
