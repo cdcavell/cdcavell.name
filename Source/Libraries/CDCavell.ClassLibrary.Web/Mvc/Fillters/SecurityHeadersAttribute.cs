8427a10b0964e838a557aff15ffe137ae3e03824
Christopher D. Cavell <cdcavell@gmail.com>
2020-10-11 16:45:57 -0500

    1.0.0 - Initial Development (#33)
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Deveopment
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development

--- Source/Libraries/CDCavell.ClassLibrary.Web/Mvc/Fillters/SecurityHeadersAttribute.cs
+++ Source/Libraries/CDCavell.ClassLibrary.Web/Mvc/Fillters/SecurityHeadersAttribute.cs
@@ -43,7 +43,7 @@ namespace CDCavell.ClassLibrary.Web.Mvc.Fillters
                 }
 
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-                var csp = "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
+                var csp = "default-src 'self'; img-src 'self' data:; object-src 'none'; frame-ancestors 'self'; sandbox allow-modals allow-forms allow-same-origin allow-scripts allow-popups; base-uri 'self';";
                 // also consider adding upgrade-insecure-requests once you have HTTPS in place for production
                 //csp += "upgrade-insecure-requests;";
                 // also an example if you need client images to be displayed from twitter
