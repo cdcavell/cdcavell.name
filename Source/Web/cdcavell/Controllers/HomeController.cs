e0fecd2df10171d6f5b073a57ee6cb87a2618581
Christopher D. Cavell <cdcavell@gmail.com>
2020-10-10 23:48:27 -0500

    1.0.0 - Initial Development (#27)
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Deveopment
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development
    
    * 1.0.0 - Initial Development

--- Source/Web/cdcavell/Controllers/HomeController.cs
+++ Source/Web/cdcavell/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
-﻿using cdcavell.Models.Search;
+﻿using cdcavell.Models.AppSettings;
+using cdcavell.Models.Search;
 using CDCavell.ClassLibrary.Web.Mvc.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
 using System.Web;
 
 namespace cdcavell.Controllers
@@ -17,23 +20,28 @@ namespace cdcavell.Controllers
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 1.0.0 | 10/02/2020 | Initial build |~ 
+    /// | Christopher D. Cavell | 1.0.0 | 10/10/2020 | Initial build |~ 
     /// </revision>
     public class HomeController : WebBaseController<HomeController>
     {
+        private readonly AppSettings _appSettings;
+
         /// <summary>
         /// Constructor method
         /// </summary>
         /// <param name="logger">ILogger</param>
         /// <param name="webHostEnvironment">IWebHostEnvironment</param>
         /// <param name="httpContextAccessor">IHttpContextAccessor</param>
+        /// <param name="appSettings">AppSettings</param>
         /// <method>HomeController(ILogger&lt;HomeController&gt; logger, IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)</method>
         public HomeController(
             ILogger<HomeController> logger,
             IWebHostEnvironment webHostEnvironment,
-            IHttpContextAccessor httpContextAccessor
+            IHttpContextAccessor httpContextAccessor,
+            AppSettings appSettings
         ) : base(logger, webHostEnvironment, httpContextAccessor)
         {
+            _appSettings = appSettings;
         }
 
         /// <summary>
@@ -98,16 +106,32 @@ namespace cdcavell.Controllers
             if (ModelState.IsValid)
             {
                 string request = HttpUtility.UrlEncode(model.SearchRequest).Clean();
+                string url = _appSettings.Authentication.BingCustomSearch.Url
+                    + "?q=" + request + "&customconfig=" + _appSettings.Authentication.BingCustomSearch.CustomConfigId;
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _appSettings.Authentication.BingCustomSearch.SubscriptionKey);
+
+                var httpResponseMessage = client.GetAsync(url).Result;
+                if (!httpResponseMessage.IsSuccessStatusCode)
+                {
+                    model.MessageClass = "text-danger";
+                    model.Message = httpResponseMessage.StatusCode
+                        + ": " + httpResponseMessage.ReasonPhrase;
+                }
+                else
+                {
+                    var responseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    SearchResponse response = JsonConvert.DeserializeObject<SearchResponse>(responseContent);
+                }
             }
             else
             {
-                if (!string.IsNullOrEmpty(model.SearchRequest))
-                {
-                    return BadRequest();
-                }
-
                 model.MessageClass = "text-danger";
-                model.Message = "No results returned";
+                if (string.IsNullOrEmpty(model.SearchRequest))
+                    model.Message = "No results returned";
+                else
+                    model.Message = "Invalid request";
             }
 
             return View(model);
