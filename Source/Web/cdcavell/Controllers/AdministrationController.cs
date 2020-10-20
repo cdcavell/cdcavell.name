fa6a42903b083d734e8e3036d5293228f0e8eb91
Christopher Cavell <cdcavell@gmail.com>
2020-10-19 19:09:07 -0500

    1.0.0 - Initial Development

--- Source/Web/cdcavell/Controllers/AdministrationController.cs
+++ Source/Web/cdcavell/Controllers/AdministrationController.cs
@@ -1,10 +1,13 @@
-﻿using cdcavell.Models.AppSettings;
+﻿using AspNetCore.SEOHelper.Sitemap;
+using cdcavell.Models.AppSettings;
 using CDCavell.ClassLibrary.Web.Mvc.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace cdcavell.Controllers
 {
@@ -15,7 +18,7 @@ namespace cdcavell.Controllers
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 1.0.0 | 10/12/2020 | Initial build |~ 
+    /// | Christopher D. Cavell | 1.0.0 | 10/19/2020 | Initial build |~ 
     /// </revision>
     [Authorize(Policy = "Administration")]
     public class AdministrationController : ApplicationBaseController<AdministrationController>
@@ -55,5 +58,31 @@ namespace cdcavell.Controllers
             return View();
         }
 
+        /// <summary>
+        /// sitemap.xml in ASP.NET Core
+        /// &lt;br /&gt;&lt;br /&gt;
+        /// https://www.c-sharpcorner.com/article/create-and-configure-sitemap-xml-in-asp-net-core/
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <method>public IActionResult CreateSitemap()</method>
+        [HttpGet]
+        public IActionResult CreateSitemap()
+        {
+            var url = "https://cdcavell.name";
+            var list = new List<SitemapNode>();
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.9, Url = url, Frequency = SitemapFrequency.Daily });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.8, Url = url + "/Home/Index", Frequency = SitemapFrequency.Daily });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.8, Url = url + "/Home/Search", Frequency = SitemapFrequency.Daily });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.7, Url = url + "/Home/PrivacyPolicy", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.7, Url = url + "/Home/TermsOfService", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.6, Url = url + "/Home/Revoke?provider=microsoft", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.6, Url = url + "/Home/Revoke?provider=google", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.6, Url = url + "/Home/Revoke?provider=github", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.6, Url = url + "/Home/Revoke?provider=twitter", Frequency = SitemapFrequency.Yearly });
+            list.Add(new SitemapNode { LastModified = DateTime.UtcNow, Priority = 0.6, Url = url + "/Home/Revoke?provider=facebook", Frequency = SitemapFrequency.Yearly });
+
+            new SitemapDocument().CreateSitemapXML(list, _webHostEnvironment.ContentRootPath);
+            return Redirect("/sitemap.xml");  // "sitemap.xml file should be created in root directory"
+        }
     }
 }
