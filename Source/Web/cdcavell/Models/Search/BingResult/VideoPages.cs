6e24897abed55bfe8739eb4ef90bafa26dbca620
Christopher Cavell <cdcavell@gmail.com>
2020-10-26 19:01:06 -0500

    1.0.0 - Issue #120 - New *_InnerHtml JQuery function

--- Source/Web/cdcavell/Models/Search/BingResult/VideoPages.cs
+++ Source/Web/cdcavell/Models/Search/BingResult/VideoPages.cs
@@ -11,7 +11,7 @@ namespace cdcavell.Models.Search.BingResult
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 1.0.0 | 10/23/2020 | Initial build |~ 
+    /// | Christopher D. Cavell | 1.0.0 | 10/26/2020 | Initial build |~ 
     /// </revision>
     public class VideoPages
     {
@@ -19,12 +19,6 @@ namespace cdcavell.Models.Search.BingResult
         public string _type { get; set; }
         /// <value>string</value>
         public string webSearchUrl { get; set; }
-        /// <value>int</value>
-        public int totalEstimatedMatches { get; set; }
-        /// <value>int</value>
-        public int nextOffset { get; set; }
-        /// <value>int</value>
-        public int currentOffset { get; set; }
         /// <value>VideoPage[]</value>
         public VideoPage[] value { get; set; } = new VideoPage[0];
     }
