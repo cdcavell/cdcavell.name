d814d30f024e6b0d59be137740afa262be0c76dc
Christopher Cavell <cdcavell@gmail.com>
2020-10-28 17:50:23 -0500

    1.0.0 - Issue #120 - Initial Development

--- Source/Web/cdcavell/wwwroot/js/site.js
+++ Source/Web/cdcavell/wwwroot/js/site.js
@@ -5,7 +5,7 @@
   *  ----------------------------------------------------------------------------------------------------
   * | Contributor           | Build | Revison Date | Description 
   * |-----------------------|-------|--------------|-----------------------------------------------------
-  * | Christopher D. Cavell | 1.0.0 | 10/26/2020   | Initial build 
+  * | Christopher D. Cavell | 1.0.0 | 10/28/2020   | Initial build 
   *
   */
 
@@ -14,34 +14,34 @@ console.log('Licensed under MIT (https://github.com/cdcavell/cdcavell.name/blob/
 
 $(function() {
 
-    console.log('-- DOM Ready');
+    console.info('-- DOM Ready');
     $('#processing').modal('hide');
 
     $('#processing').on('show.bs.modal', function (e) {
-        console.log('-- show processing modal');
+        console.info('-- show processing modal');
     })
 
     $('#processing').on('shown.bs.modal', function (e) {
-        console.log('-- processing modal shown');
+        console.info('-- processing modal shown');
     })
 
     $('#processing').on('hide.bs.modal', function (e) {
-        console.log('-- hide processing modal');
+        console.info('-- hide processing modal');
     })
 
     $('#processing').on('hidden.bs.modal', function (e) {
-        console.log('-- processing modal hid');
+        console.info('-- processing modal hid');
     })
 
     $('#form').on('submit', function (e) { 
         var data = $("#form :input").serializeArray();
-        console.log('-- form submitted');
-        console.log(data); 
+        console.info('-- form submitted');
+        console.debug(data); 
 
         if ($('#form').attr('suppressSubmit') != 'false') {
             e.preventDefault();
             alert('Form submission suppressed while in development');
-            console.log('-- form submit suppressed');
+            console.info('-- form submit suppressed');
         } else {
             $('#processing').modal('show');
         }
@@ -51,6 +51,6 @@ $(function() {
 
 $(window).on('load', function() {
 
-    console.log('-- Document Ready');
+    console.info('-- Document Ready');
 
 });
