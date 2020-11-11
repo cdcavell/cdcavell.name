4252a904ebb387fe6bd631b15cb0f166827469fe
Christopher Cavell <cdcavell@gmail.com>
2020-11-11 17:09:21 -0600

    1.0.0.9 - Implement Registration/Roles/Permissions [#183]

--- Source/Web/cdcavell/Data/Registration.cs
+++ Source/Web/cdcavell/Data/Registration.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1.Esf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace cdcavell.Data
 {
@@ -14,7 +16,7 @@ namespace cdcavell.Data
     /// __Revisions:__~~
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
-    /// | Christopher D. Cavell | 1.0.0.9 | 11/03/2020 | Implement Registration/Roles/Permissions [#183](https://github.com/cdcavell/cdcavell.name/issues/183) |~ 
+    /// | Christopher D. Cavell | 1.0.0.9 | 11/11/2020 | Implement Registration/Roles/Permissions [#183](https://github.com/cdcavell/cdcavell.name/issues/183) |~ 
     /// </revision>
     [Table("Registration")]
     public class Registration : DataModel<Registration>
@@ -22,34 +24,45 @@ namespace cdcavell.Data
         /// <value>string</value>
         [Required]
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
         /// <value>string</value>
         [Required]
         [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         /// <value>string</value>
         [Required]
         [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         /// <value>DateTime?</value>
         [AllowNull]
         [DataType(DataType.DateTime)]
+        [Display(Name = "Request Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? RequestDate { get; set; } = DateTime.MinValue;
         /// <value>DateTime?</value>
         [AllowNull]
         [DataType(DataType.DateTime)]
+        [Display(Name = "Approved Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? ApprovedDate { get; set; } = DateTime.MinValue;
         /// <value>string</value>
         [AllowNull]
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "Approved By")]
         public string ApprovedBy { get; set; }
         /// <value>DateTime?</value>
         [AllowNull]
         [DataType(DataType.DateTime)]
+        [Display(Name = "Revoked Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? RevokedDate { get; set; } = DateTime.MinValue;
         /// <value>string</value>
         [AllowNull]
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "Revoked By")]
         public string RevokedBy { get; set; }
         /// <value>bool</value>
         [NotMapped]
@@ -62,6 +75,46 @@ namespace cdcavell.Data
                         return true;
 
                 return false;
+            }
+        }
+        /// <value>bool</value>
+        [NotMapped]
+        public bool IsPending
+        {
+            get
+            {
+                if (ApprovedDate == DateTime.MinValue)
+                    if (RevokedDate == DateTime.MinValue)
+                        return true;
+
+                return false;
+            }
+        }
+        /// <value>bool</value>
+        [NotMapped]
+        public bool IsRevoked
+        {
+            get
+            {
+                if (RevokedDate != DateTime.MinValue)
+                    return true;
+
+                return false;
+            }
+        }
+        /// <value>string</value>
+        [NotMapped]
+        public string Status
+        {
+            get 
+            {
+                if (IsActive)
+                    return "Active";
+
+                if (IsRevoked)
+                    return "Revoked";
+
+                return "Pending";
             }
         }
 
