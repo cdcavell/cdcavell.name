9ae018aed67cd69734df4a717d676c413bc64384
Christopher Cavell <cdcavell@gmail.com>
2020-11-12 22:14:40 -0600

    1.0.0.9 - Implement Registration/Roles/Permissions [#183]

--- Source/Web/is4-cdcavell/Models/AppSettings/AppSettings.cs
+++ Source/Web/is4-cdcavell/Models/AppSettings/AppSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace is4_cdcavell.Models.AppSettings
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
     /// | Christopher D. Cavell | 1.0.0.0 | 10/20/2020 | Initial build |~ 
+    /// | Christopher D. Cavell | 1.0.0.9 | 11/12/2020 | Implement Registration/Roles/Permissions [#183](https://github.com/cdcavell/cdcavell.name/issues/183) |~ 
     /// </revision>
     public class AppSettings
     {
@@ -26,6 +28,11 @@ namespace is4_cdcavell.Models.AppSettings
         public string AssemblyVersion
         {
             get { return Assembly.GetEntryAssembly().GetName().Version.ToString(); }
+        }
+        /// <value>DateTime</value>
+        public DateTime LastModifiedDate
+        {
+            get { return File.GetLastWriteTime(Assembly.GetEntryAssembly().Location); }
         }
         /// <value>ConnectionStrings</value>
         public ConnectionStrings ConnectionStrings { get; set; }
