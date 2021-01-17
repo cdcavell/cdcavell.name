a06f61908e55cc437d6445144f0163b7eda805b6
Christopher Cavell <cdcavell@gmail.com>
2021-01-16 19:16:39 -0600

    1.0.2.0 - Initial build Duende IdentityServer5

---
+++ Source/Web/dis5-cdcavell/Filters/SecurityHeadersAttribute.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See https://duendesoftware.com/license/identityserver.pdf for license information.
+
+using CDCavell.ClassLibrary.Web.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace dis5_cdcavell.Filters
+{
+    /// <summary>
+    /// Security Headers Attribute Filter 
+    /// &lt;br /&gt;&lt;br /&gt;
+    /// Copyright (c) Duende Software. All rights reserved.
+    /// See https://duendesoftware.com/license/identityserver.pdf for license information. 
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 1.0.2.0 | 01/16/2021 | Initial build |~ 
+    /// </revision>
+    public class SecurityHeadersAttribute : ActionFilterAttribute
+    {
+        private string _StyleNonce;
+        private string _ScriptNonce;
+
+        /// <summary>
+        /// Executes before result execution
+        /// </summary>
+        /// <param name="context">ResultExecutingContext</param>
+        /// <method>OnResultExecuting(ResultExecutingContext context)</method>
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+            if (result is ViewResult)
+            {
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+                if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Type-Options"))
+                {
+                    context.HttpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                }
+
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+                if (!context.HttpContext.Response.Headers.ContainsKey("X-Frame-Options"))
+                {
+                    context.HttpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                }
+
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+                var csp = "default-src 'self'; ";
+                csp += "img-src 'self' data:; ";
+                csp += "object-src 'none'; ";
+                csp += "frame-ancestors 'none'; ";
+                csp += "sandbox allow-forms allow-same-origin allow-scripts allow-popups; ";
+                csp += "base-uri 'self'; ";
+                csp += "style-src 'self' 'nonce-" + _StyleNonce + "'; ";
+                csp += "script-src 'strict-dynamic' https: 'self' 'nonce-" + _ScriptNonce + "'; ";
+                // also consider adding upgrade-insecure-requests once you have HTTPS in place for production
+                csp += "upgrade-insecure-requests;";
+                // also an example if you need client images to be displayed from twitter
+                // csp += "img-src 'self' https://pbs.twimg.com;";
+
+                // once for standards compliant browsers
+                if (!context.HttpContext.Response.Headers.ContainsKey("Content-Security-Policy"))
+                {
+                    context.HttpContext.Response.Headers.Add("Content-Security-Policy", csp);
+                }
+                // and once again for IE
+                if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Security-Policy"))
+                {
+                    context.HttpContext.Response.Headers.Add("X-Content-Security-Policy", csp);
+                }
+
+                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+                var referrer_policy = "no-referrer";
+                if (!context.HttpContext.Response.Headers.ContainsKey("Referrer-Policy"))
+                {
+                    context.HttpContext.Response.Headers.Add("Referrer-Policy", referrer_policy);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executes after action method execution to set script nonce
+        /// </summary>
+        /// <param name="context">ActionExecutedContext</param>
+        /// <method>OnActionExecuted(ActionExecutedContext context)</method>
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var controller = context.Controller as Controller;
+            _StyleNonce = Nonce.Calculate();
+            _ScriptNonce = Nonce.Calculate();
+            controller.ViewBag.StyleNonce = _StyleNonce;
+            controller.ViewBag.ScriptNonce = _ScriptNonce;
+            base.OnActionExecuted(context);
+        }
+    }
+}
--- Source/Web/dis5-cdcavell/Startup.cs
+++ Source/Web/dis5-cdcavell/Startup.cs
@@ -1,25 +1,76 @@
+using CDCavell.ClassLibrary.Commons.Logging;
+using CDCavell.ClassLibrary.Web.Mvc.Filters;
+using dis5_cdcavell.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace dis5_cdcavell
 {
+    /// <summary>
+    /// The Startup class configures services and the application's request pipeline&lt;br /&gt;&lt;br /&gt;
+    /// _Services_ are components that are used by the app. For example, a logging component is a service. Code to configure (_or register_) services is added to the ```Startup.ConfigureServices``` method.&lt;br /&gt;&lt;br /&gt;
+    /// The request handling pipeline is composed as a series of _middleware_ components. For example, a middleware might handle requests for static files or redirect HTTP requests to HTTPS. Each middleware performs asynchronous operations on an ```HttpContext``` and then either invokes the next middleware in the pipeline or terminates the request. Code to configure the request handling pipeline is added to the ```Startup.Configure``` method.
+    /// </summary>
+    /// <revision>
+    /// __Revisions:__~~
+    /// | Contributor | Build | Revison Date | Description |~
+    /// |-------------|-------|--------------|-------------|~
+    /// | Christopher D. Cavell | 1.0.2.0 | 01/16/2021 | Initial build |~ 
+    /// </revision>
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private IConfiguration _configuration;
+        private IWebHostEnvironment _webHostEnvironment;
+        private Logger _logger;
+
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        /// <param name="configuration">IConfiguration</param>
+        /// <param name="webHostEnvironment">IWebHostEnvironment</param>
+        /// <method>Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)</method>
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
+            _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /// <summary>
+        /// This optional method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">IServiceCollection</param>
+        /// <method>ConfigureServices(IServiceCollection services)</method>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddControllersWithViews();
+
+            // Register IHttpContextAccessor
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Register controller fillters
+            services.AddScoped<SecurityHeadersAttribute>();
+            services.AddScoped<ControllerActionLogFilter>();
+            services.AddScoped<ControllerActionUserFilter>();
+            services.AddScoped<ControllerActionPageFilter>();
+        }
+
+        /// <summary>
+        /// This required method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">IApplicationBuilder</param>
+        /// <param name="env">IWebHostEnvironment</param>
+        /// <param name="logger">ILogger&lt;Startup&gt;</param>
+        /// <param name="lifetime">IHostApplicationLifetime</param>
+        /// <param name="dbContext">AspIdUsersDbContext</param>
+        /// <method>Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger&lt;Startup&gt; logger, IHostApplicationLifetime lifetime, AspIdUsersDbContext dbContext)</method>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger, IHostApplicationLifetime lifetime)
         {
             if (env.IsDevelopment())
             {
