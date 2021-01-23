3ac6dd4c72820195b323c2e38a06376bbec61b29
Christopher Cavell <cdcavell@gmail.com>
2021-01-23 08:42:15 -0600

    1.0.3.0 - Initial build Authorization Service

--- Source/Web/cdcavell/Startup.cs
+++ Source/Web/cdcavell/Startup.cs
@@ -52,7 +52,7 @@ namespace cdcavell
     /// | Christopher D. Cavell | 1.0.0.7 | 10/31/2020 | Integrate Bing’s Adaptive URL submission API with your website [#144](https://github.com/cdcavell/cdcavell.name/issues/144) |~ 
     /// | Christopher D. Cavell | 1.0.0.9 | 11/11/2020 | Implement Registration/Roles/Permissions [#183](https://github.com/cdcavell/cdcavell.name/issues/183) |~ 
     /// | Christopher D. Cavell | 1.0.2.2 | 01/18/2021 | Convert GrantType from Implicit to Pkce |~ 
-    /// | Christopher D. Cavell | 1.0.3.0 | 10/22/2020 | Initial build Authorization Service |~ 
+    /// | Christopher D. Cavell | 1.0.3.0 | 10/23/2020 | Initial build Authorization Service |~ 
     /// </revision>
     public class Startup
     {
