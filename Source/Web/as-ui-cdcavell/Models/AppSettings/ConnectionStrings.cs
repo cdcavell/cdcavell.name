79937b28bf00a9993993d8a1c6720bd1f234467a
Christopher Cavell <cdcavell@gmail.com>
2021-02-07 15:38:52 -0600

    1.0.3.1 - Add ApplicationInsights

--- Source/Web/as-ui-cdcavell/Models/AppSettings/ConnectionStrings.cs
+++ Source/Web/as-ui-cdcavell/Models/AppSettings/ConnectionStrings.cs
@@ -8,12 +8,13 @@
     /// | Contributor | Build | Revison Date | Description |~
     /// |-------------|-------|--------------|-------------|~
     /// | Christopher D. Cavell | 1.0.3.0 | 02/05/2021 | Initial build Authorization Service |~ 
+    /// | Christopher D. Cavell | 1.0.3.1 | 02/07/2021 | Add ApplicationInsights |~
     /// </revision>
     public class ConnectionStrings
     {
         /// <value>string</value>
         public string AuthorizationUiConnection { get; set; }
         /// <value>string</value>
-        public string RedisCacheConnection { get; set; }
+        public string ApplicationInsightsConnection { get; set; }
     }
 }
